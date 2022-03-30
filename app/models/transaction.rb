7a7459157f546aa6cef4b5633d2568b3a1b89af9
Ben-glitch-cloud <BTLawrence44@gmail.com>
2022-03-30 10:51:46 +0100

    Updated tests for the validating the model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,7 +1,7 @@
 class Transaction < ApplicationRecord 
-    validates :customer_id, presence: true 
+    validates :customer_id, presence: true, numericality: { only_integer: true }
     validates :currency_input, presence: true
-    validates :amount_input, presence: true
+    validates :amount_input, presence: true, comparison: { greater_than: 0 }, numericality: true
     validates :currency_output, presence: true
-    validates :amount_output, presence: true
+    validates :amount_output, presence: true, comparison: { greater_than: 0 }, numericality: true
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,12 +1,32 @@
 require 'rails_helper'
 
-RSpec.describe Transaction, type: :model do
+RSpec.describe Transaction, type: :model do  
   it 'should create a transaction when all values are their' do  
     expect(Transaction.new(customer_id: 04, currency_input: 'USD', amount_input: 100, currency_output: 'EUR', amount_output: 45)).to be_valid 
+  end   
+
+  it 'should throw an error if customer id is not a number' do  
+    expect(Transaction.new(customer_id: 'HTG', currency_input: 'USD', amount_input: 100, currency_output: 'EUR', amount_output: 45)).to be_invalid
+  end 
+
+  it 'should throw an error when all value are not their' do  
+    expect(Transaction.new(customer_id: nil, currency_input: 'USD', amount_input: 100, currency_output: 'EUR', amount_output: 45)).to be_invalid
   end  
 
-  it 'should though an error when all value are not their' do  
-    expect(Transaction.new(customer_id: nil, currency_input: 'USD', amount_input: 100, currency_output: 'EUR', amount_output: 45)).to be_invalid
-  end 
+  it 'should throw an error if amount input is 0' do  
+    expect(Transaction.new(customer_id: nil, currency_input: 'USD', amount_input: 0, currency_output: 'EUR', amount_output: 45)).to be_invalid
+  end  
+
+  it 'should throw an error if amount output is 0' do  
+    expect(Transaction.new(customer_id: 01, currency_input: 'USD', amount_input: 10, currency_output: 'EUR', amount_output: 0)).to be_invalid
+  end   
+
+  it 'should throw an error if amount input is a string' do  
+    expect(Transaction.new(customer_id: 01, currency_input: 'USD', amount_input: 'NULL', currency_output: 'EUR', amount_output: 0)).to be_invalid
+  end   
+
+  it 'should throw an error if amount output is a string' do  
+    expect(Transaction.new(customer_id: 01, currency_input: 'USD', amount_input: 10, currency_output: 'EUR', amount_output: 'NULL')).to be_invalid
+  end  
 
 end
