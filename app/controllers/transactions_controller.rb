144167e71c623bbd2ad4e463cc5a54653975fbe0
Ben-glitch-cloud <BTLawrence44@gmail.com>
2022-04-06 15:28:04 +0100

    customer transactions can be edited

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -15,14 +15,31 @@ class TransactionsController < ApplicationController
   end   
 
   def create   
-    @transaction = Transaction.new(transaction_params) 
+    @transaction = Transaction.new(transaction_params)  
+    puts @transaction
     if @transaction.save
       redirect_to '/transactions/showAll' 
     else
       render :new, status: :unprocessable_entity
     end
+  end  
+
+  def edit 
+    @transaction = Transaction.find(params[:id])
+  end  
+
+  def update  
+    @transaction = Transaction.find(params[:id])
+
+    if @transaction.update(transaction_params)
+      redirect_to '/transactions/showAll'
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end 
+
 end 
+
 
 def transaction_params
   params.require(:transaction).permit(:customer_id, :currency_input, :amount_input, :currency_output, :amount_output)
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do 
-  resources :transaction
+  
   root "transactions#index" 
 
   get "/transactions/index", to: "transactions#index" 
   get "/transactions/showAll", to: "transactions#showAll" 
   get "/transactions/new", to: "transactions#new"  
-  get "/transactions/:id", to: "transactions#show" 
+  get "/transactions/:id", to: "transactions#show"  
+  get "/transactions/:id/edit", to: 'transactions#edit' 
+  patch "/transactions/:id", to: 'transactions#update'
   post "/transactions/create", to: "transactions#create" 
 end
